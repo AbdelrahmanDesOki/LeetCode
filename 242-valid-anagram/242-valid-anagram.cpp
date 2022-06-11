df2ac5418a26e8e5c59eb5d6e7fcf45a7ed9d0a8
Desokii <81819045+AbdelrahmanDesOki@users.noreply.github.com>
2022-06-11 13:26:07 +0200

    Time: 25 ms (12.18%), Space: 7.4 MB (46.51%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-     int count=0;
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         
-//         for(int i=0;i<t.length();i++){
-//              if(t[i]==s[i]){
-//                  count++;
-                
-//              }   
-            
-//         }
-        
-//         if(count==s.length())
-//             return true;
-            
         return s==t;    
     }
 };
