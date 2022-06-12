9cdaa664da3bd278cf00ee5c0041c6d63bbf31d5
Desokii <81819045+AbdelrahmanDesOki@users.noreply.github.com>
2022-06-13 01:58:18 +0200

    Time: 21 ms (8.24%), Space: 13.7 MB (72.45%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -13,20 +13,5 @@ public:
     }
         return best;
         
-        
-//     int max_ending_here = arr[0];
- 
-//     int min_ending_here = arr[0];
- 
-//     int max_so_far = arr[0];
-  
-//     for (int i = 1; i < n; i++)
-//     {
-//         int temp = max({arr[i], arr[i] * max_ending_here, arr[i] * min_ending_here});
-//         min_ending_here = min({arr[i], arr[i] * max_ending_here, arr[i] * min_ending_here});
-//         max_ending_here = temp;
-//         max_so_far = max(max_so_far, max_ending_here);
-//     }
-//     return max_so_far;
     }
 };
