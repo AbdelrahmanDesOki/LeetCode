d9465a22465e211246eaf3a8eb9bd538999ddd0d
Desokii <81819045+AbdelrahmanDesOki@users.noreply.github.com>
2022-08-10 01:20:32 +0200

    Time: 18 ms (91.22%), Space: 45.8 MB (33.74%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        //Arrays.sort(coins);
+    int [] few = new int[amount + 1];
+        Arrays.fill(few, amount + 1);
+    few[0] = 0;
+    for(int i=0;i<=amount; i++) {
+        for(int j = 0; j < coins.length; j++) {
+            if(coins[j]<= i){
+            few[i] = Math.min(few[i], 1 + few[i - coins[j]]);
+            } 
+            //else {break;}
+        }   
+}
+
+
+return few[amount] > amount ? -1 : few[amount];
+    }    
+}
